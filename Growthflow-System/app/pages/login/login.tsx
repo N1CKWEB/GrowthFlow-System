3df5ab74bf8c4d064bdc4fac03bd6806ade9ac92
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2025-11-20 17:14:25 -0300

    Avanzamos con en el login luego de mucho!

--- Growthflow-System/app/pages/login/login.tsx
+++ Growthflow-System/app/pages/login/login.tsx
@@ -1,35 +1,48 @@
-// src/App.jsx
-import { useEffect, useState } from "react";
-import { supabase } from "../../supabaseClient";
+'use client';
+import * as Field from '../../components/Field';
+import "../../styles/login.css";
 
 
 
-function Login() {
-  const [leads, setLeads] = useState([]);
-
-  useEffect(() => {
-    getLeads();
-  }, []);
-
-  async function getLeads() {
-    const { data, error } = await supabase.from("leads").select();
-    if (error) console.error(error);
-    else setLeads(data);
-  }
+export default function Login() {
+  const logo = "/assets/img/logo_negro.png";
 
   return (
-    <div>
-      <h1>Login</h1>
-      <h1>Leads Capturados</h1>
-      <ul>
-        {leads.map((lead:any) => (
-          <li key={lead.id}>
-            {lead.username} - {lead.email || "No email"} - {lead.telefono || "No teléfono"}
-          </li>
-        ))}
-      </ul>
+    <div className="login-container">
+      {/* LEFT COLUMN */}
+      <div className="login-left">
+        <img src={logo} className="login-logo" alt="GrowthFlow" />
+
+        <div className="login-form-wrapper">
+          <p className="login-subtitle">Inicia Sesión para gestionar tus negocios </p>
+      <Field.Root name="serverName">
+        <Field.Label>Gmail</Field.Label>
+        <Field.Control
+          defaultValue=""
+          placeholder="Introduce tu correo electronico"
+          required
+          className={'bg-red-300'}
+          minLength={3}
+          pattern=".*[A-Za-z].*"
+        />
+        <Field.Description>Must be 3 or more characters long</Field.Description>
+        <Field.Error />
+      </Field.Root>
+
+        </div>
+      </div>
+
+      {/* RIGHT COLUMN */}
+      <div className="login-right">
+        <div className="hero-overlay">
+          <h1 className="hero-title">
+            It’s time to make management easier and faster ⚡
+          </h1>
+          <p className="hero-subtitle">
+            Manage attendance, leave, employee data, payroll and more — all in one simple app.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Login;
