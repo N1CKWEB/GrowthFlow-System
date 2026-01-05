edd3da13688a0d8d95e237404b28456e3c867502
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2026-01-05 16:16:03 -0300

    Avances del modulo o sección de home

--- Growthflow-System/app/components/StartCard.tsx
+++ Growthflow-System/app/components/StartCard.tsx
@@ -2,31 +2,39 @@ type Props = {
   title: string;
   value: string;
   trend?: string;
+  img?: string;
 };
 
-export function StatCard({ title, value, trend }: Props) {
+export function StatCard({ title, value, trend, img }: Props) {
+  img = "/assets/img/forward.png";
+
   return (
     <div
-      className="rounded-2xl
+      className="rounded-xl
   bg-linear-to-br from-slate-800 to-slate-900
-  p-6
+  p-5
   shadow-[0_10px_30px_rgba(0,0,0,0.35)]
-  border border-white/5"
+  border border-white/5 w-70 h-40"
     >
-      <p className="text-sm text-slate-400">{title}</p>
+      <p className="text-xl text-slate-400 relative left-4 top-7">{title}</p>
 
-      <div className="mt-2 flex items-end justify-between">
-        <span className="text-2xl font-semibold text-white">{value}</span>
+      <div className="mt-2 flex items-start justify-between">
+        <span className="text-3xl font-semibold text-white relative top-11 left-5 justify-center">
+          {value}
+        </span>
 
         {trend && (
           <span
-            className={`text-sm ${
+            className={`text-sm relative top-21 right-27 ${
               trend.startsWith("-") ? "text-red-400" : "text-green-400"
             }`}
           >
             {trend}
           </span>
         )}
+        <div className="bg-blue-600 rounded-full relative bottom-3 right-2">
+          <img src={img} alt="" />
+        </div>
       </div>
     </div>
   );
